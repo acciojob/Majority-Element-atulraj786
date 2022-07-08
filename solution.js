ed2566f7a80acb6d3bde2cfdfadf2e93bf419dc6
atulraj786 <107700182+atulraj786@users.noreply.github.com>
2022-07-09 00:23:15 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // arr = array of numbers
 function major(arr)
 {
-  //your code here
-}
+    var obj = {};
+    
+    for(var i = 0; i < arr.length; i++){
+        obj[arr[i]] = obj[arr[i]] + 1 || 1;
+        if(obj[arr[i]] > arr.length / 2)  return arr[i];
+    }
+    
+ }
 module.exports=major;
